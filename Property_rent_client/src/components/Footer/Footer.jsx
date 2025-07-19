a5b0b2faef2423b157045e8585b5594f0bcdfa43
Shubham7488 <115517849+Shubham7488@users.noreply.github.com>
2025-07-19 22:03:20 +0530

    removed personal name

--- Property_rent_client/src/components/Footer/Footer.jsx
+++ Property_rent_client/src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
                             </div>
                             <div>
                                 <p className="text-sm text-white">
-                                    &copy; Copyright 2025. All Rights Reserved by Shubham Kumar.
+                                    &copy; Copyright 2025. All Rights Reserved.
                                 </p>
                             </div>
                         </div>
